517e7b1852bdb15ca2e493ac43445c522f7e9622
CrankyBear <jeremie.bozant@ipilyon.net>
2025-04-27 18:33:34 +0200

    fix: error message duplication

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
 const userNameInput = document.getElementById('prenom');
-const submitButton = document.getElementById("btn-submit");
+const submitButton = document.getElementById('btn-submit');
+
+const handleFormNameError = () => {
+    if (!isUserNameInputFilled()) {
+        userNameInput.classList.add('is-invalid');
+        displayErrorMessage();
+    } else {
+        localStorage.setItem('username', userNameInput.value);
+        window.location.href = './tasks.html';
+    }
+};
+
+const isUserNameInputFilled = () => {
+    return userNameInput.value.trim() !== '';
+};
+
+const displayErrorMessage = () => {
+    // Prevents error message duplication
+    if (!document.querySelector('.form-group span')) {
+        const errorSpan = document.createElement('span');
+        errorSpan.textContent = 'Veuillez saisir un nom';
+        errorSpan.style.color = 'red';
+        document.querySelector('.form-group').appendChild(errorSpan);
+    }
+};
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     handleFormNameError();
-})
-
-handleFormNameError = () => {
-    if(!isUserNameInputFilled()) {
-        userNameInput.classList.add("is-invalid");
-        errorMessage();
-    } else {
-        localStorage.setItem("username", userNameInput.value);
-        window.location.href = './tasks.html';
-    }
-}
-
-isUserNameInputFilled = () => {
-    return userNameInput.value === '' ? false : true;
-}
-
-errorMessage = () => {
-    const errorSpan = document.createElement('span');
-    errorSpan.innerHTML = 'Veuillez saisir un nom';
-    errorSpan.style.color = 'red';
-    document.querySelector('.form-group').appendChild(errorSpan);
-}
+});
